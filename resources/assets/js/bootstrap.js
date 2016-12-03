886a9733a97be6529e5215c10e7daf4bcc63604b
Joshua Partogi <Joshua Partogi>
2016-12-03 23:35:16 +0800

    Change to rollup again

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -6,7 +6,8 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-window.$ = window.jQuery = require('jquery');
+// window.$ = window.jQuery = require('jquery');
+require('jquery');
 require('bootstrap-sass');
 //require('foundation-sites');
 
