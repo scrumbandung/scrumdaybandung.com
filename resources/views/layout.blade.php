d8803c773ee4dcae728b35ea1a923aa761ddbc90
Joshua Partogi <Joshua Partogi>
2016-12-03 13:44:53 +0800

    Initial layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Scrum Day Bandung 2017 - @yield('title')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" />
+    </head>
+    <body>
+    <div class="top-bar">
+        <div class="top-bar-left">
+            <ul class="menu">
+                <li class="menu-text">Scrum Day Bandung 2017</li>
+            </ul>
+        </div>
+        <div class="top-bar-right">
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Speakers</a></li>
+                <li><a href="#">Schedule</a></li>
+                <li><a href="#">Venue</a></li>
+                <li><a href="#">Sponsors</a></li>
+                <li><a href="#">Travel Guide</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+    @yield('content')
+    </body>
+</html>
