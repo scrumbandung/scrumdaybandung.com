7e91f4259a2d28159e8130a01424cd790af9c25c
Joshua Partogi <Joshua Partogi>
2016-12-26 13:11:51 +0800

    Add SEO stuffs

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -11,7 +11,7 @@
 Scrum Day Bandung 2017 is an intense 1-day business agility &amp; management conference with Scrum as its framework. But we think Scrum Day Bandung is not just a conference. We think it is a community movement towards software development ecosystem that emphasizes agility and professionalism. We think software is a business needs to be competitive in 21st century. We think agility should be extended beyond IT because agility is a business needs not a methodology. The event is a perfect place to be for leaders, managers and change agents who have concerns about agility and profesionalism in software development ecosystem to meet, exchange experiences and support each other. Unlike other conferences in the region where participants only leave the event with information or knowledge, in this event participants will feel inspired and empowered to make changes in their respective companies. 
                 </p>
                 <p>
-This will be an exclusive event and the first business &amp; management conference with Scrum in the region. If you share our mission for an software development ecosystem that emphasizes agility and professionalism, come and join the movement and attend this 1-day event.                    
+This will be an exclusive event and the first business &amp; management conference with Scrum in the region. If you share our mission for an software development ecosystem that emphasizes agility and professionalism, come and join the movement and attend this 1-day event. Join us to be part of this historic moment. We wish to see you at the event.                  
                 </p>
         </div>
     </div>
