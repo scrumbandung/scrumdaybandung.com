4389257d7c0fcf10e4bec3d2be1e2a5b5d638f7c
Joshua Partogi <Joshua Partogi>
2016-12-27 13:45:08 +0800

    Add more wording for about event

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -15,7 +15,10 @@
 </p>
 
                 <p>
-Scrum Day Bandung 2017 is an intense 1-day business agility &amp; management conference with Scrum as its framework. But we think Scrum Day Bandung is not just a conference. We think it is a community movement towards software development ecosystem that emphasizes agility and professionalism. We think software is a business needs to be competitive in 21st century. We think agility should be extended beyond IT because agility is a business needs not a methodology. The event is a perfect place to be for leaders, managers and change agents who have concerns about agility and profesionalism in software development ecosystem to meet, exchange experiences and support each other. Unlike other conferences in the region where participants only leave the event with information or knowledge, in this event participants will feel inspired and empowered to make changes in their respective companies. 
+Scrum Day Bandung 2017 is an intense 1-day business agility &amp; management conference with Scrum as its framework. But we think Scrum Day Bandung is more than just a conference. We think it is a community movement towards software development ecosystem that emphasizes agility and professionalism. We think software is a business need for companies to be competitive in 21st century. We think agility should be extended beyond IT because agility is a business requirement not a software development methodology. 
+</p>
+<p>
+Scrum Day Bandung is a perfect place to be for leaders, managers and change agents who have concerns about business agility and profesionalism in software development ecosystem to meet, exchange experiences and support each other. Unlike other conferences in the region where participants only leave the event with information or knowledge, in this event participants will feel inspired and empowered to make changes in their respective companies knowing that others will walk alongside them. 
                 </p>
                 <p>
 This event will be an exclusive event and the first business &amp; management conference with Scrum in the region. If you share our mission for an software development ecosystem that emphasizes agility and professionalism, join the movement and attend this 1-day annual event. We wish to see you at the event.                  
@@ -30,9 +33,9 @@ This event will be an exclusive event and the first business &amp; management co
             		<strong>Time</strong>: 09:00-17:00<br/>
 
             		<h4>Admission Fee</h4>
-            		<p>Free for 20 persons who has registered for the conference.</p>
+            		<p>Free introduction to Scrum Training limited to 20 persons who have registered for the conference.</p>
             	</div>
-            	<div class="col-md-5 col-md-offset-1 schedule-box">
+            	<div class="col-md-6 col-md-offset-1 schedule-box">
             		<h3>Scrum Day Bandung 2017</h3>
             		<strong>28 February 2017</strong><br/>
             		<strong>Time</strong>: 09:00-17:30<br/>
@@ -40,12 +43,29 @@ This event will be an exclusive event and the first business &amp; management co
             		<h4>Admission Fee</h4>
             		<p><strong>Early Bird (limited)</strong>: IDR 500,000,-</p>
             		<p><strong>Regular</strong>: IDR 700,000,-</p>
+
+                    <h3>The sessions</h3>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h4>Talks</h4>
+                            <p>
+                            Inspring talks for business leaders to lead their companies with a more relevant management model for 21st centuries situation from other radical thought leaders.</p>
+                        </div>
+                        <div class="col-md-12">
+                            <h4>Coaches Corner</h4>
+                            <p>A booth for Scrum newbies to ask all things about Scrum and Agile management who will be answered by experienced Scrum coaches.</p>
+                        </div>
+                        <div class="col-md-12">
+                            <h4>Open Space</h4>
+                            <p>Open discussion about radical or controversial topics around Scrum or Agile management facilitated by experienced Scrum coaches.</p>
+                        </div>
+                    </div>
             	</div>
             </div>
 
             <h2>The audiences</h2>
             <h3>Corporate Leaders (C-Level)</h3>
-            <p>Corporate leaders has a vital role to initiate change and make change persistent in the company for the long run. Corporate leaders attending this conference are expected to be inspired and bring an Agile mindset and support professionalism in their respective companies.
+            <p>Corporate leaders has a vital role to initiate change towards Agility and make the change persistent in the company. Corporate leaders attending this conference are expected to be inspired and bring an Agile mindset and support professionalism in their respective companies.
             </p>
 
             <h3>Product Owners</h3>
