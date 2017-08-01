a94d6d43317726e4c0a30094a697ed45e7910a5d
Joshua Partogi <jpartogi@live.com>
2017-08-01 14:06:14 +0800

    Enhance call for presentation

--- resources/views/call-for-presentation.blade.php
+++ resources/views/call-for-presentation.blade.php
@@ -28,11 +28,11 @@
 				<p>
 				<strong>Guidelines for your presentation:</strong>
 				<ul>
-					<li>No sales &amp; no marketing about your brand or your product. The organiser will ban any speaker who does not respect this rule.</li>
+					<li>No sales &amp; no marketing about your brand or your product but case study is encouraged.. The organiser will ban any speaker who does not respect this rule.</li>
 					<li>You are not allowed to use your opportunity on the stage for hiring people. The organiser will ban any speaker who does not respect this rule.</li>
 					<li>Your presentation must relate to Scrum. You can talk about any other Agile practices but you must explain how the Agile practice fits within the Scrum framework.</li>
 					<li>Make sure the Scrum mechanics, values and principles in your presentation is according to <a href="http://www.scrumguides.org">Scrum Guide</a>. We don't want to confuse the audience with your own version of Scrum.</li>	
-					<li>No Scrum basics. We only want high quality talks. If it's revolutionary, we would like to hear it. We're looking for a talk where the Scrum framework can be exploited to increase organisation's Agility.</li>							
+					<li>No Scrum basics. We want high quality talks. If it's revolutionary, we would like to hear it. We're looking for a talk where the Scrum framework can be exploited to help organisations increase their Agility.</li>							
 				</ul>
 				</p>
 
@@ -43,7 +43,7 @@
 				<li><strong>Submission closed</strong>: October 31st 2017</li>
 				<li><strong>Selection announcement</strong>: Mid November 2017</li>
 				</ul>
-				The selection process will take some time. Please be patient to wait for the announcement email from the programme committee. We will send you an email if your talk is selected. If your talk is selected by the programme committee, you are expected to support your own travel and accommodation to and from Bandung, Indonesia. We would like stay independent hence not relying on big sponsors and keep the cost affordable for the participants. We would like to keep Scrum Day Bandung purely for learning from each other's experience.  
+				The selection process will take some time. We're not only looking for quality talks but we also look at you and your contribution to the Agile software development community. Please be patient to wait for the announcement email from the programme committee. We will send you an email if your talk is selected. If your talk is selected by the programme committee, you are expected to support your own travel and accommodation to and from Bandung, Indonesia. We would like stay independent hence not relying on big sponsors and keep the cost affordable for the participants. We would like to keep Scrum Day Bandung purely for learning from each other's experience.  
 				</p>
 
 			</div>
