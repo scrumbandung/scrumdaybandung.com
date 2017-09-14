6e1a970c2d587d92109c7e4db57b7cdf8c894d0d
Joshua Partogi <jpartogi@live.com>
2017-09-14 10:36:51 +0800

    Add more text for volunteer page

--- resources/views/volunteer.blade.php
+++ resources/views/volunteer.blade.php
@@ -8,9 +8,29 @@
 <div id="resources" class="container push-top">
 	<div class="row">
 		<div class="col-md-12">
-			<h1 class="page-header">Panitia Scrum Day Bandung 2018</h1>
+			<h1 class="page-header">Rekrutmen terbuka Panitia Scrum Day Bandung 2018</h1>
 
-			Panitia Scrum Day Bandung 2018 mencari agen perubahan dari Bandung dan sekitarnya untuk membantu kami dalam misi kami menciptakan ekosistem software development yang ideal di Indonesia. Simak video berikut untuk mendengarkan alasan kenapa kami semua termotivasi untuk menjadi panitian Scrum Day Bandung 2017. Setelah itu silahkan isi form di bawah.
+			<p>
+				<ul>
+					<li>Apakah kamu mendambakan sebuah ekosistem software development dimana kostumernya kolaboratif dengan software developer, berpikiran terbuka, pengertian, bisa menerima ketidak-pastian, tidak memaksakan kehendaknya sendiri, mau memahami kompleksitas pekerjaan software development dan mengutamakan profesionalisme di atas <em>deadline</em>?</li>
+					<li>Apakah kamu mendambakan sebuah ekosistem software development dimana software developer mau mengambil inisiatif &amp; <em>self-organised</em>, berpikiran terbuka, berani mengemukakan pendapat, tidak egois &amp; komitmen terhadap kesepakatan tim, transparan dalam pekerjaannya, berani keluar dari zona nyaman dan mau selalu belajar teknik-teknik terkini dalam menghantarkan software berkualitas?</li>
+					<li>Apakah kamu mendambakan sebuah ekosistem software development dimana pimpinan perusahaan &amp; manajernya <em>people person</em>, rendah hati, memimpin dengan melayani, tidak otoriter, tidak politis &amp; birokratis, tidak hanya peduli menjaga jabatannya saja, berani melakukan hal tidak popular demi kebaikan banyak orang, dapat menjadi coach &amp; mentor bagi software developer, menciptakan lingkungan kerja dimana orang-orang aman mengambil resiko dan menjunjung tinggi profesionalisme di atas <em>deadline</em>?</li>
+				</ul>
+			</p>
+
+			<p>
+				Secara umum berikutlah gambaran dari ekosistem software development ideal yang kami dambakan: <br/>
+				<img src="/img/ideal_ecosystem.png" width="50%"/>
+			</p>
+
+			<p>
+
+				Realitanya secara umum kita belum melihat keadaan tersebut di ekosistem software development di Indonesia. Bahkan di daerah-daerah keadaannya bisa lebih buruk lagi. Mungkin kita tidak dapat merubah semuanya menjadi ideal. Mungkin kita tidak akan melihat perubahan di generasi kita. Tetapi setidaknya kita menjadi bagian dari sejarah yang telah berusaha membuat perubahan bukan hanya sekedar berpangku tangan berharap perubahan turun dari surga.
+			</p>
+
+			<p>
+			Panitia Scrum Day Bandung 2018 saat ini sedang mencari agen perubahan dari Bandung dan sekitarnya untuk membantu kami dalam mencapai misi kami menciptakan ekosistem software development yang ideal di Indonesia. Misi besar membutuhkan banyak orang-orang yang berhati besar. Perubahan ini tentunya kita juga yang akan menikmatinya. Simak video berikut untuk mendengarkan alasan kenapa kami semua termotivasi untuk menjadi panitia Scrum Day Bandung 2017. Bila kalian terinspirasi oleh motivasi kami silahkan isi form di bawah dan tunggu email balasan dari kami untuk bertemu dan berbincang-bincang di sebuah kafe di sekitar Bandung mengenai harapan dan komitmen kamu di acara ini. Scrum On!
+			</p>
 
 			<div class="panel panel-default">
                 <div class="panel-body">
